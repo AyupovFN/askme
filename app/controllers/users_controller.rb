a51d3ead8006c096b0fb8d83ffa6054e3f5bc887
AyupovFN <kapterka@bk.ru>
2020-03-26 00:56:44 +0300

    Views refactoring part3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,15 @@
 class UsersController < ApplicationController
   def index
+    @users = [
+        User.new(
+            id: 1,
+            name: 'Vadim',
+            username: 'installero',
+            avatar_url: 'https://secure.gravatar.com/avatar/' \
+          '71269686e0f757ddb4f73614f43ae445?s=100'
+        ),
+        User.new(id: 2, name: 'Misha', username: 'aristofun')
+    ]
   end
 
   def new
@@ -12,10 +22,12 @@ class UsersController < ApplicationController
     @user = User.new(
         name: 'Vadim',
         username: 'installero',
-    avatar_url: 'https://secure.gravatar.com/avatar/712696e0f757ddb4f73614f43ae445?s=100'
+    #avatar_url: 'https://secure.gravatar.com/avatar/712696e0f757ddb4f73614f43ae445?s=100'
     )
     @questions = [
-        Question.new(text: 'how are you?', created_at: Date.parse('25.03.2020'))
+        Question.new(text: 'how are you?', created_at: Date.parse('25.03.2020')),
+        Question.new(text: 'what is a sense of life?', created_at: Date.parse('25.03.2020'))
     ]
+    @new_question = Question.new
   end
 end
