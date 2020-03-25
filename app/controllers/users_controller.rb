3a8042a338ebda4b3db0e3817ad2dbbcb2b84c1c
AyupovFN <kapterka@bk.ru>
2020-03-25 18:25:22 +0300

    add css, helpers, partials part2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,10 @@ class UsersController < ApplicationController
     @user = User.new(
         name: 'Vadim',
         username: 'installero',
-        avatar_url: 'https://secure.gravatar.com/avatar/712696e0f757ddb4f73614f43ae445?s=100'
+    avatar_url: 'https://secure.gravatar.com/avatar/712696e0f757ddb4f73614f43ae445?s=100'
     )
+    @questions = [
+        Question.new(text: 'how are you?', created_at: Date.parse('25.03.2020'))
+    ]
   end
 end
