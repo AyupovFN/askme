072f940b1914996df43415318335ccfdc9ac8234
AyupovFN <kapterka@bk.ru>
2020-03-25 16:38:06 +0300

    made views part1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,18 @@
 class UsersController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
   def show
-    @time = Time.now
-    @hello = "Привет!!!"
+    @user = User.new(
+        name: 'Vadim',
+        username: 'installero',
+        avatar_url: 'https://secure.gravatar.com/avatar/712696e0f757ddb4f73614f43ae445?s=100'
+    )
   end
 end
