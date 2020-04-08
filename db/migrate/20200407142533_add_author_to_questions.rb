f07043739e5799fe3c22bb81fee1398400e55aeb
AyupovFN <kapterka@bk.ru>
2020-04-08 18:19:32 +0300

    Fix type author_id, add date/time questions/answer

--- db/migrate/20200407142533_add_author_to_questions.rb
+++
@@ -1,6 +0,0 @@
-class AddAuthorToQuestions < ActiveRecord::Migration[5.2]
-  def change
-    add_column :questions, :author_id, :string
-    add_index :questions, :author_id
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_07_142533) do
+ActiveRecord::Schema.define(version: 2020_04_08_115825) do
 
   create_table "questions", force: :cascade do |t|
     t.string "text"
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 2020_04_07_142533) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
-    t.string "author_id"
+    t.integer "author_id"
     t.index ["author_id"], name: "index_questions_on_author_id"
     t.index ["user_id"], name: "index_questions_on_user_id"
   end
