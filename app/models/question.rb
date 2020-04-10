d4dd76ce7e73dd67a7df81016cab955d86212a52
AyupovFN <kapterka@bk.ru>
2020-04-10 15:17:25 +0300

    Fix input author questions and answer

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,6 +2,6 @@ class Question < ApplicationRecord
   belongs_to :user
   belongs_to :author, class_name: 'User', foreign_key: 'author_id', optional: true
 
-  validates :text, :user,  presence: true
-  validates :text, length: { maximum: 255 }
+  validates :text, :user, presence: true
+  validates :text, presence: true, length: { maximum: 255 }
 end
