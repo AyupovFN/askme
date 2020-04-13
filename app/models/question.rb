734104c9e49ccbea25a2aa04364b1e45c3da15e5
AyupovFN <kapterka@bk.ru>
2020-04-13 17:22:10 +0300

    indentation changes

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,5 +2,5 @@ class Question < ApplicationRecord
   belongs_to :user
   belongs_to :author, class_name: 'User', foreign_key: 'author_id', optional: true
 
-   validates :text, presence: true, length: { maximum: 255 }
+  validates :text, presence: true, length: { maximum: 255 }
 end
