96815ee449d783668fbd30ed47bcd303b751fbd3
AyupovFN <kapterka@bk.ru>
2020-04-13 14:01:39 +0300

    Fix validates question

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,5 @@
 class QuestionsController < ApplicationController
-  before_action :load_question, only: [ :edit, :update, :destroy]
+  before_action :load_question, only: [:edit, :update, :destroy]
   before_action :authorize_user, except: [:create]
 
   def edit
@@ -8,7 +8,7 @@ class QuestionsController < ApplicationController
   # POST /questions
   def create
     @question = Question.new(question_params)
-     @question.author = current_user
+    @question.author = current_user
 
     if @question.save
       redirect_to user_path(@question.user), notice: 'Вопрос создан.'
@@ -44,10 +44,7 @@ class QuestionsController < ApplicationController
   end
 
   def question_params
-    # Защита от уязвимости: если текущий пользователь — адресат вопроса,
-    # он может менять ответы на вопрос, ему доступно также поле :answer.
-    if current_user.present? &&
-        params[:question][:user_id].to_i == current_user.id
+    if current_user.present? && params[:question][:user_id].to_i == current_user.id
       params.require(:question).permit(:user_id, :text, :answer)
     else
       params.require(:question).permit(:user_id, :text)
--- app/models/question.rb
+++ app/models/question.rb
@@ -2,6 +2,5 @@ class Question < ApplicationRecord
   belongs_to :user
   belongs_to :author, class_name: 'User', foreign_key: 'author_id', optional: true
 
-  validates :text, :user, presence: true
-  validates :text, presence: true, length: { maximum: 255 }
+   validates :text, presence: true, length: { maximum: 255 }
 end
